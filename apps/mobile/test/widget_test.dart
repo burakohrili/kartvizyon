17680f5897b0ae3cb0be2b64b8e9802511382636
burakohrili <105482577+burakohrili@users.noreply.github.com>
2026-09-07 13:22:38 +0300

    Use router back dispatch in account deletion widget test

--- apps/mobile/test/widget_test.dart
+++ apps/mobile/test/widget_test.dart
@@ -155,7 +155,7 @@ void main() {
     expect(find.text('Vazgeç'), findsOneWidget);
     await tester.tap(find.text('Vazgeç'));
     await tester.pumpAndSettle();
-    await tester.pageBack();
+    await tester.binding.handlePopRoute();
     await tester.pumpAndSettle();
     // Gizlilik ve ses saklama bilgi kartları listeyi uzattı.
     await tester.scrollUntilVisible(
